d355bf4ea2e851d383165f8edcd6f0108fc30b12
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-05-13 23:42:35 +0900

    Design: alarm flatlist custom toggle

--- src/components/FriendsList.jsx
+++ src/components/FriendsList.jsx
@@ -1,20 +1,36 @@
 import React from "react";
+import { useState } from "react";
 import { View, Text, StyleSheet, Switch } from "react-native";
+import ToggleSwitch from "rn-toggle-switch";
 
-const CustomRow = ({ title, description, image_url }) => (
-  <View style={styles.container}>
-    <Image source={{ uri: image_url }} style={styles.photo} />
-    <View style={styles.container_text}>
-      <Text style={styles.title}>{title}</Text>
-      <Text style={styles.description}>{description}</Text>
+const CustomRow = ({ time, label, activate }) => {
+  // switch toggle state
+  const [isEnabled, setIsEnabled] = useState({ activate });
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.container_text}>
+        <Text style={styles.title}>{time}</Text>
+        <Text style={styles.label}>{label}</Text>
+      </View>
+
+      <Switch
+        trackColor={{ false: "#AFAFAF", true: "#6d61ff" }}
+        thumbColor={isEnabled ? "#fff" : "#fff"}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
     </View>
-  </View>
-);
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
+    width: 325,
     flex: 1,
-    width: 310,
+    height: 85,
     flexDirection: "row",
     padding: 10,
     marginLeft: 16,
@@ -36,14 +52,6 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     marginLeft: 12,
     justifyContent: "center",
-  },
-  description: {
-    fontSize: 11,
-    fontStyle: "italic",
-  },
-  photo: {
-    height: 50,
-    width: 50,
   },
 });
 
